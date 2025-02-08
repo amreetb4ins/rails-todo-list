fd991fef5e0b9f957b8686b1e9fd7dd19462276c
Amreet Bains <amreet8@hotmail.co.uk>
2025-02-08 14:38:39 +0000

    added form in todos view and updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get '/todos', to: 'todos#index'
+  resources :todos, only: [ :index, :create ]
 end
